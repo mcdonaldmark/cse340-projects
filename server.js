4ec359eb075d5503426e92dafc51e9f9910e6b9d
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-08 17:37:17 -0700

    commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,11 +25,30 @@ app.use((req, res, next) => {
 app.get("/", baseController.buildHome);
 
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
 
 // 404 route (if no other route matches)
 app.use((req, res) => {
   res.status(404).render("index", { title: "404 - Page Not Found" });
+});
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
 });
 
 // Start the server

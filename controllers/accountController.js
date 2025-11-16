6b98f5a690de2b5368750279a09409a0047ab6cf
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-16 01:44:41 -0700

    commit

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,15 +1,28 @@
 const utilities = require("../utilities/")
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
+ *  Deliver login view
+ * *************************************** */
 async function buildLogin(req, res, next) {
   let nav = await utilities.getNav()
   res.render("account/login", {
     title: "Login",
-      nav,
+    nav,
     notice: req.flash("notice")
   })
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+*  Deliver registration view
+* *************************************** */
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null,
+    notice: req.flash("notice")
+  })
+}
+
+module.exports = { buildLogin, buildRegister }
--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,15 @@
 // script.js
 console.log("CSE Motors site loaded successfully!");
+
+const passwordInput = document.getElementById('password');
+const toggleButton = document.getElementById('togglePassword');
+
+toggleButton.addEventListener('click', () => {
+if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    toggleButton.textContent = 'Hide';
+} else {
+    passwordInput.type = 'password';
+    toggleButton.textContent = 'Show';
+    }
+});

ca03ac4af72eeba8d35c7d927a16fc1285aafba0
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-16 20:32:57 -0700

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
+const regValidate = require('../utilities/account-validation');
 const accountController = require("../controllers/accountController");
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
